24087fc73ed487894003578ce9df60177adf6a39
abubakr iz <168574041+abubakriz@users.noreply.github.com>
2024-05-07 12:26:03 +0200

    Add simple calculator

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,92 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+import Button from "./components/Button.vue";
 
-<template>Hello World</template>
+const OPERANDS = ["7", "8", "9", "4", "5", "6", "1", "2", "3", ".", "0"];
+const OPERATIONS = ["+", "-", "*", "/", "=", "AC"];
+const expression = ref("");
 
-<style scoped></style>
+function addOperation(op) {
+  if (op == "AC") {
+    expression.value = "";
+    return;
+  }
+
+  if (OPERATIONS.includes(expression.value.at(-1))) {
+    return;
+  }
+
+  switch (op) {
+    case "=":
+      expression.value = String(eval(expression.value));
+      return;
+  }
+
+  expression.value += op;
+}
+
+function addOpperand(num) {
+  expression.value += num;
+}
+</script>
+
+<template>
+  <div class="container">
+    <output class="result">{{ expression }}</output>
+    <div class="seperator">
+      <div class="numbers-grid">
+        <Button v-for="op in OPERANDS" @click="addOpperand(op)">{{
+          op
+        }}</Button>
+      </div>
+
+      <div class="operations-grid">
+        <Button
+          v-for="op in OPERATIONS"
+          @click="addOperation(op)"
+          :class="{ 'btn--accent': op == '=' }"
+          >{{ op }}</Button
+        >
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: grid;
+  place-content: center;
+  min-height: 100vh;
+  gap: 1rem;
+}
+
+.numbers-grid {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  max-width: min-content;
+  gap: 0.5rem;
+}
+
+.operations-grid {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  max-width: min-content;
+  gap: 0.5rem;
+}
+
+.seperator {
+  display: grid;
+  grid-auto-flow: column;
+  place-items: start;
+  max-width: min-content;
+  gap: 2rem;
+}
+
+.result {
+  font-size: 3rem;
+  max-width: min-content;
+  font-family: monospace;
+  color: #f2e7dc;
+  height: 1em;
+}
+</style>
---
+++ src/components/Button.vue
@@ -0,0 +1,33 @@
+<script setup></script>
+
+<template>
+  <button class="btn">
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped>
+.btn {
+  border-radius: 0.5em;
+  border: 4px solid #027373;
+  color: #f2e7dc;
+  font-weight: black;
+  background-color: #038c7f;
+  color: #f2e7dc;
+  font-size: 1.44rem;
+  cursor: pointer;
+  transition: transform 250ms ease;
+
+  min-width: 3em;
+  min-height: 3em;
+}
+
+.btn:hover {
+  transform: scale(0.95);
+}
+
+.btn--accent {
+  background-color: #d55906;
+  border: 4px solid #be3406;
+}
+</style>
